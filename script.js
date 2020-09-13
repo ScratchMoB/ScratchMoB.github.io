8b818ab15a7b3dc5666f9bd12c999b4d48f3e34a
jemunm378 <64645131+jemunm378@users.noreply.github.com>
2020-09-13 16:23:15 -0400

    Update script.js

--- script.js
+++ script.js
@@ -30,4 +30,21 @@ fetch("https://scratchdb.lefty.one/v2/user/info" + user).then(res => res.json())
   fetch("https://scratch.mit.edu/site-api/comments/user/mres/").then(res => res.text()).then((data) => {
     document.getElementById("comments: ").innerHTML ="comments: " + data 
   });
+
+fetch("https://scratchdb.lefty.one/v2/user/info/mres").then(res => res.json()).then((data) => {
+    document.getElementById("work: ").innerHTML ="work: " + data.work
+  });
+
+fetch("https://scratchdb.lefty.one/v2/user/info/mres").then(res => res.json()).then((data) => {
+    document.getElementById("followers: ").innerHTML ="followers: " + data.followers
+
+  });
+
+fetch("https://scratchdb.lefty.one/v2/user/info/mres").then(res => res.json()).then((data) => {
+    document.getElementById("following: ").innerHTML ="following: " + data.following
+  });
+
+
+
+
 });
